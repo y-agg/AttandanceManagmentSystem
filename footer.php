72d45ef023e971afc378a3be3ac31b36cc38fc1b
Yash Aggarwal <yash.aggarwal@vips.edu>
2020-10-26 19:53:00 +0530

    info updated

--- footer.php
+++ footer.php
@@ -2,26 +2,25 @@
     <div class="fbg_resize">
       <div class="col c1">
         <h2><span>Image</span> Gallery</h2>
-        <a href="#"><img src="img/University-students.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="img/hero-ivy-league-college-student-male-reading.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="img/y5gv_1_7.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal6.jpg" width="75" height="75" alt="" class="gal" /></a> </div>
+        <a href="#"><img src="img/University-students.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="img/hero-ivy-league-college-student-male-reading.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="img/y5gv_1_7.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal6.jpg" width="75" height="75" alt="" class="gal" /></a>
+      </div>
       <div class="col c2">
         <h2><span>Services</span> Overview</h2>
-       <ul class="fbg_ul">
-  Attendance capture pattern and tracking designed for Lectures, track and manage attendance in all the days at any time. It is using RF ID card, punch card and scanners to systematic the attendance recording procedure. Attendance reports can be generated in various criteria and assist teachers about student entry details and ease to calculate percentage of leaves, present date, gives relevant systematic information to the administration department and checks the individual attendance record and alert to parents
-
-
-</br>
+        <ul class="fbg_ul">
+          Attendance capture pattern and tracking designed for Lectures, track and manage attendance in all the days at any time. It is using RF ID card, punch card and scanners to systematic the attendance recording procedure. Attendance reports can be generated in various criteria and assist teachers about student entry details and ease to calculate percentage of leaves, present date, gives relevant systematic information to the administration department and checks the individual attendance record and alert to parents
+          </br>
         </ul>
       </div>
       <div class="col c3">
         <h2><span>Contact</span> Us</h2>
-       <p class="contact_info"> <span>Address:</span><br />
-         <b> Project Name : Attendance Management System</b><br/><br/>
-                                              
-                                                Phone:  (011) 25351765<br/><br/>
-                                                Mobile:  8375952989<br/><br/>
-                                                Email:  aastha.aggarwal2@gmail.<br/><br/>
-                                                
- </p>
+        <p class="contact_info"> <span>Address:</span><br />
+          <b> Project Name : Attendance Management System</b><br /><br />
+
+          Phone: (011) 1234567<br /><br />
+          Mobile: 1234567890<br /><br />
+          Email: AttendanceManagementSystem@gmail.<br /><br />
+
+        </p>
       </div>
       <div class="clr"></div>
     </div>
