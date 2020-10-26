b0a5c2697f1b40ebeb5540b8c56ebcfe99993851
Yash Aggarwal <yash.aggarwal@vips.edu>
2020-10-26 19:42:41 +0530

    connection modification

--- connection/dbconfig.php
+++ connection/dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-   $selected = mysqli_connect("localhost","root","pwdpwd","dbattandance");
+   $selected = mysqli_connect("localhost","root","","dbattandance");
    if (mysqli_connect_errno($selected)){
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
